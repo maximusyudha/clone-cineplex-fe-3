076bb566df2344c6e1d35f89d27fa53ad4ff1035
Maximus Yudha <Maximusyudha781@gmail.com>
2023-03-22 22:09:22 +0700

    Menambahkan navbar

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const path = require("path");
 
-const imageDomains = [
-  "",
-];
+const imageDomains = [""];
 
 const nextConfig = {
   reactStrictMode: true,
@@ -12,6 +10,11 @@ const nextConfig = {
     includePaths: [path.join(__dirname, "styles")],
   },
   images: { domains: imageDomains },
+};
+module.exports = {
+  images: {
+    domains: ["21cineplex.com"],
+  },
 };
 
 module.exports = nextConfig;
--- src/components/ExampleLayout.tsx
+++ src/components/ExampleLayout.tsx
@@ -1,11 +1,45 @@
-import Link from "next/link";
+/* eslint-disable @next/next/no-img-element */
+import Image from "next/image";
 
 const ExampleLayout = ({ children }: any) => {
   return (
     <>
-      <div className="fixed z-50 w-full bg-white h-[64px] p-5 border border-b">
-        Header
-      </div>
+      <nav className="bg-gray-900">
+        <div className="max-w-100xl mx-auto px-2 sm:px-6 lg:px-8">
+          <div className="relative flex items-center justify-between h-20">
+            <div className="absolute inset-y-0 left-0 flex items-center">
+              <a href="#" className="flex-shrink-0 text-white">
+                <img
+                  src="https://21cineplex.com//theme/v5/assets/img/logo.png"
+                  width={100}
+                  height={200}
+                  alt="Logo"
+                />
+              </a>
+              <div className="ml-10 flex items-baseline">
+                <a
+                  href="#"
+                  className="px-3 py-2 rounded-md text-sm font-medium text-white bg-gray-800 focus:outline-none focus:text-white focus:bg-gray-700"
+                >
+                  Home
+                </a>
+                <a
+                  href="#"
+                  className="ml-4 px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
+                >
+                  About
+                </a>
+                <a
+                  href="#"
+                  className="ml-4 px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
+                >
+                  Contact
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
       <div className="pt-[88px] pb-4 px-2 md:px-5">{children}</div>
       <div className="w-full bg-gray-50 h-[100px] pb-[48px] md:pb-0 px-2 md:px-5">
         Footer
