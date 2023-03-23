f5f3dc5f2d9d82f0a3871542c61595590fc2aa5f
Maximus Yudha <Maximusyudha781@gmail.com>
2023-03-23 10:51:30 +0700

    menambah now playing dkk

--- src/components/ExampleLayout.tsx
+++ src/components/ExampleLayout.tsx
@@ -4,42 +4,53 @@ import Image from "next/image";
 const ExampleLayout = ({ children }: any) => {
   return (
     <>
-      <nav className="bg-gray-900">
+      <nav className="bg-teal-900">
         <div className="max-w-100xl mx-auto px-2 sm:px-6 lg:px-8">
-          <div className="relative flex items-center justify-between h-20">
+          <div className="relative fixed items-center justify-between h-20">
             <div className="absolute inset-y-0 left-0 flex items-center">
               <a href="#" className="flex-shrink-0 text-white">
                 <img
                   src="https://21cineplex.com//theme/v5/assets/img/logo.png"
-                  width={100}
+                  width={200}
                   height={200}
                   alt="Logo"
                 />
               </a>
-              <div className="ml-10 flex items-baseline">
-                <a
-                  href="#"
-                  className="px-3 py-2 rounded-md text-sm font-medium text-white bg-gray-800 focus:outline-none focus:text-white focus:bg-gray-700"
-                >
-                  Home
-                </a>
-                <a
-                  href="#"
-                  className="ml-4 px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
-                >
-                  About
-                </a>
-                <a
-                  href="#"
-                  className="ml-4 px-3 py-2 rounded-md text-sm font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
-                >
-                  Contact
-                </a>
-              </div>
             </div>
           </div>
         </div>
       </nav>
+      <div className="max-w-100xl mx-auto px-2 sm:px-6 lg:px-8 bg-gray-100">
+        <div className="relative flex items-center justify-between h-20">
+          <div className="absolute inset-y-0 left-0 flex items-center"></div>
+          <div className="ml-100  flex items-baseline">
+            <a
+              href="#"
+              className="px-3 py-2 rounded-md text-sm font-medium text-green focus:outline-none focus:text-green hover:text-white hover:bg-gray-700  focus:text-white focus:bg-gray-700"
+            >
+              Now Playing
+            </a>
+            <a
+              href="#"
+              className="ml-4 px-3 py-2 rounded-md text-sm font-medium text-green-900 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
+            >
+              Teather
+            </a>
+            <a
+              href="#"
+              className="ml-4 px-3 py-2 rounded-md text-sm font-medium text-green-900 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
+            >
+              Promotion
+            </a>
+            <a
+              href="#"
+              className="ml-4 px-3 py-2 rounded-md text-sm font-medium text-green-900 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700"
+            >
+              Info 21
+            </a>
+          </div>
+        </div>
+      </div>
       <div className="pt-[88px] pb-4 px-2 md:px-5">{children}</div>
       <div className="w-full bg-gray-50 h-[100px] pb-[48px] md:pb-0 px-2 md:px-5">
         Footer
